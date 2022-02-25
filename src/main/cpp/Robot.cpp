ee88094ab2c3987002ad389ca7b4973edd936035
jaapvanbergeijk <info@jaapvanbergeijk.nl>
2022-02-24 21:17:36 -0600

    autonomous test: drive to fetch a red ball

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -45,7 +45,7 @@ void Robot::RobotInit()
   // where x is a number from 0 to 15 (track maximum 16 objects)
   //
   // Possible values for Label are: "red ball", "blue ball", "person", "robot"
-  // Possible values for Status are: "TRACKED", "LOST"
+  // Possible values for Status are: "TRACKED", "LOST", "REMOVED"
   // Location is defined as an array with x,y,z location relative to camera in mm: [x,y,z]
   //----------------------------------------------------
   auto nt_inst = nt::NetworkTableInstance::GetDefault();
@@ -129,21 +129,56 @@ void Robot::AutonomousInit()
 void Robot::AutonomousPeriodic()
 {
 //  DriveWithJoystick(false);
+  double m_closest_x = 0.0;
+  double m_closest_y = 0.0;
+  double m_closest_z = 1000.0;
+  uint8_t n_red_balls = 0;
 
   for (uint8_t i=0; i<FRONT_CAM_MAX_OBJECTS; i++)
   {
     if ((nte_front_cam_object_label[i].GetString("none") == "red ball") &&
         (nte_front_cam_object_status[i].GetString("none") == "TRACKED"))
     {
+      n_red_balls++;
+
       std::vector<double> arr = nte_front_cam_object_location[i].GetDoubleArray(std::vector<double>());
       double m_x = arr[0];
       double m_y = arr[1];
       double m_z = arr[2];
 
-      std::cout << "First Red Ball at: " << m_x << ", " << m_y << ", " << m_z << std::endl;
+      std::cout << "Red Ball " << n_red_balls << " at: " << m_x << ", " << m_y << ", " << m_z << std::endl;
 
-      i = FRONT_CAM_MAX_OBJECTS;
+      if (m_z < m_closest_z)
+      {
+        m_closest_x = m_x;
+        m_closest_y = m_y;
+        m_closest_z = m_z;
+      }
+
+      if (n_red_balls >= 3)
+        i = FRONT_CAM_MAX_OBJECTS;
     }
+  }
+
+  if (n_red_balls >= 1)
+  {
+    // turn and drive towards closest red_ball
+    auto xSpeed = m_xspeedLimiter.Calculate(0.0) * Drivetrain::kMaxSpeed;
+    auto rot = m_rotLimiter.Calculate(0.0) * Drivetrain::kMaxAngularSpeed;
+
+    if (m_closest_z > 800.0)
+      xSpeed = ((m_closest_z - 800.0) * 0.03) * Drivetrain::kMaxSpeed;
+
+    if (m_closest_x < -10.0)
+      rot = -(m_closest_x * 0.02) * Drivetrain::kMaxAngularSpeed;
+    else if (m_closest_x > -10.0)
+      rot = -(m_closest_x * 0.02) * Drivetrain::kMaxAngularSpeed;
+
+    m_drive.Drive(xSpeed, (units::velocity::meters_per_second_t)0.0, rot, false);
+  }
+  else
+  {
+    m_drive.Drive((units::velocity::meters_per_second_t)0.0, (units::velocity::meters_per_second_t)0.0, (units::angular_velocity::radians_per_second_t)0.0, false);
   }
 }
 
